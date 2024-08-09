fd65c30319a3e35acf62f1c932d29d536d331fa9
Bhagyashri Thakur <bhagyashri.thakur@bbd.co.za>
2024-08-09 12:03:11 +0530

    add a content on above 2 files

---
+++ App.js
@@ -0,0 +1,25 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+const parent = React.createElement(
+    "div", 
+    {id : "parent"}, 
+          [ React.createElement("div", {id: "child1"},[
+            React.createElement("h1",{} , "I am a child of h1 tag"),
+            React.createElement("h2",{} , "I am a child of h2 tag"),
+         ]
+ ,          React.createElement("div", {id: "child2"},[
+            React.createElement("h1",{} , "I am a child of h1 tag"),
+            React.createElement("h2",{} , "I am a child of h2 tag"),
+ ]
+))]
+);
+console.log(parent);
+
+// const heading = React.createElement("h1" ,
+//     {id: "heading"},
+//      "Hello react World!"
+//     );
+
+const root1 = ReactDOM.createRoot(document.getElementById("root"));
+root1.render(parent);
+
